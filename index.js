ee25cb8b84b1aa1263d2918b8b8c35a5e70eba5f
Audrey Gillies <audrey.gillies@gmail.com>
2023-05-10 21:20:02 -0600

    updating the email validation to allow an empty email input; creating a function where the user is prompted if a readme file already exists so the file isn't automatically overwritten

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ const questions = [
 		message: 'Enter your preferred email address: ',
 		name: 'email',
 		validate: (input) => {
-			return input.match(/^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$/)
-				? true
-				: 'Must ve a valid email.';
+			return input && !input.match(/^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$/)
+				? 'Must ve a valid email.'
+				: true;
 		}
 	}
 ];
@@ -74,12 +74,45 @@ function makeDirectory() {
 	});
 }
 
+// function to see if a generated README file already exists - asks user if they wish to overwrite or create a new one.
+async function saveFile(readmeData) {
+	let fileName = 'README.md';
+	if (fs.existsSync(`./Generated/${fileName}`)) {
+		const overwriteQuestion = [
+			{
+				type: 'list',
+				choices: ['Overwrite existing file', 'Create new file'],
+				message:
+					'There is already a generated README.md file saved.  Do you wish to overwrite the existing readme or save this input in a new README file?',
+				name: 'overwrite'
+			}
+		];
+		await inquirer.prompt(overwriteQuestion).then((data) => {
+			const { overwrite } = data;
+			// if the user wants to create a new file - reading all files in the directory of generated files and creating a new one with digits at the end of the file name (finds the largest number in the files names and adds one to it)
+			if (overwrite !== 'Overwrite existing file') {
+				fs.readdirSync('./Generated').reduce((prev, next) => {
+					let digitFileChar = +next.split('.')[0].slice(6);
+					if (digitFileChar >= prev) {
+						fileName = `README${digitFileChar ? (digitFileChar += 1) : 1}.md`;
+					}
+					return digitFileChar;
+				}, 0);
+			}
+		});
+	}
+	writeToFile(fileName, readmeData);
+}
+
 // Create a function to initialize app
 function init() {
-	inquirer.prompt(questions).then((data) => {
+	inquirer.prompt(questions).then(async (data) => {
 		makeDirectory();
 
-		writeToFile('README.md', markdown(data));
+		// generating the markdown text
+		const readmeData = markdown(data);
+
+		saveFile(readmeData);
 	});
 }
 
